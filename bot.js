c5d208bfd02c30e9cc586ed0f7fdd1a91f082ada
Nickolans <nickolansgriffith@gmail.com>
2018-09-15 11:25:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,22 @@
 var Discord = require('discord.io');
-var logger = require('winston');
-
-
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
 
 // Initialize Discord Bot
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity(`Hey, BRUH!`);
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 7) == 'nickbot' || message.substring(0, 7) == 'Nickbot') {
-    //     bot.sendMessage({
-    //         to: channelID,
-    //         message: 'DETECTED'
-    //     });
 
         var args = message.substring(7).split(' ');
         var cmd = args[1];
        
-        args = args.splice(7);
+        //args = args.splice(7);
         switch(cmd) {
             // !ping
             case 'hello':
@@ -40,7 +26,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
             break;
             // Just add any case commands if you want to..
-            console.log("");
          }
      }
 });
