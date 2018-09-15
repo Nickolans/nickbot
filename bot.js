db5773f18ca3f768e2b97165e8a1fb8655a6ac15
Nickolans <nickolansgriffith@gmail.com>
2018-09-15 11:41:00 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,37 +3,29 @@ const Discord = require("discord.js");
 // Initialize Discord Bot
 const bot = new Discord.Client();
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`Hey, BRUH!`);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    message.toLowerCase;
-    if (message.substring(0, 7) == 'nickbot') {
-
-        var args = message.substring(7).split(' ');
+bot.on('message', msg => {
+  
+  msg.toLowerCase;
+  if (msg.substring(0, 7) == 'nickbot') {
+    var args = msg.substring(7).split(' ');
         var cmd = args[1];
        
         //args = args.splice(7);
         switch(cmd) {
             // !ping
             case 'hello':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Hey'
-                });
+                msg.reply('Hey!');
             break;
             // Just add any case commands if you want to..
          }
-     } else if (message.substring(0, 8) == 'nickbot?') {
-       bot.sendMessage({
-            to: channelID,
-            message: 'Yes shister??'
-        });
+  } else if (msg.substring(0, 8) == 'nickbot?') {
+       msg.reply('Yes shister?');
      }
+
 });
 
 bot.login(process.env.token);
