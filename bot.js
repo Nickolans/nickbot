8adf8230b3f32d06bb46c24b79125a0d536c8716
Nickolans <nickolansgriffith@gmail.com>
2018-09-15 11:57:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,29 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
 // Initialize Discord Bot
 const bot = new Discord.Client();
 
-bot.on("ready", async () => {
+bot.on('ready', async () => {
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity(`Hey, Wassup!`);
 });
 
-bot.on("message", async message => {
+bot.on('message', async message => {
   
   message.toLowerCase;
-  if (message.substring(0, 7) == "nickbot") {
+  if (message.substring(0, 7) == 'nickbot') {
     var args = message.substring(7).split(' ');
         var cmd = args[1];
         switch(cmd) {
             // !ping
             case 'hello':
-                message.channel.send("Hey!");
+                message.channel.send('Hey!');
             break;
             // Just add any case commands if you want to..
          }
-  } else if (message.substring(0, 8) == "nickbot?") {
-       message.channel.send("Yes shister?");
+  } else if (message.substring(0, 8) == 'nickbot?') {
+       message.channel.send('Yes shister?');
      }
-
 });
 
 bot.login(process.env.token);
