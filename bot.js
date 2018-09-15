abc87d2db26cae50ba9787cb664499b6acb1e0f1
Nickolans <nickolansgriffith@gmail.com>
2018-09-15 11:37:27 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 // Initialize Discord Bot
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client();
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
